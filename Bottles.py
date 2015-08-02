c895c82d517abebd0d76d9148eafc49355ee126e
lookamango <James@Jamess-MacBook-Air.local>
2015-08-02 10:44:33 -0500

    Challenge 1 - Finished
    99 Bottles of beer

--- Bottles.py
+++ Bottles.py
@@ -1,3 +1,6 @@
 def print_lyrics():
-    for i in range(99):
-        
+    for i in range(99, 0, -1):
+        print("{0} bottles of beer on the wall, {0} bottles of beer, you take one down, "
+              "pass it around! {1} bottle of beer on the wall\n".format(i, i-1))
+
+print_lyrics()
